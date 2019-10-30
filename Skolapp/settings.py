27efd61b4f7e98d9de78bf499052a1123747584b
Jonckers <etiennejonckers@gmail.com>
2019-10-30 16:11:50 +0100

    Creation de deux applications: School qui regroupera tous les models en lien avec l'école et Candidat pour les parents et élèves.

--- Skolapp/settings.py
+++ Skolapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SCHOOL.apps.SchoolConfig',
+    'CANDIDAT.apps.CandidatConfig',
+
 ]
 
 MIDDLEWARE = [
